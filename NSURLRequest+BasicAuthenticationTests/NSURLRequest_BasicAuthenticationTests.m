34a2451ec53bdd10f35c36e24be324ac2555e125
quellish <github@quellish.neomailbox.com>
2012-11-08 01:01:29 -0800

    Added additional tests demonstrating different ways of performing basic authentication

--- NSURLRequest+BasicAuthenticationTests/NSURLRequest_BasicAuthenticationTests.m
+++ NSURLRequest+BasicAuthenticationTests/NSURLRequest_BasicAuthenticationTests.m
@@ -7,6 +7,7 @@
 //
 
 #import "NSURLRequest_BasicAuthenticationTests.h"
+#import "NSMutableURLRequest+BasicAuthentication.h"
 #import <UnitTestHTTPServer/HTTPServer.h>
 #import <UnitTestHTTPServer/HTTPConnection.h>
 
@@ -17,7 +18,6 @@
 @implementation AuthenticatedConnection
 
 - (BOOL)isPasswordProtected:(NSString *)path {
-
     return YES;
 }
 
@@ -26,7 +26,6 @@
 }
 
 - (NSString *)passwordForUser:(NSString *)username {
-
     return @"secret";
 }
 
@@ -67,9 +66,86 @@
     [super tearDown];
 }
 
-- (void)testExample
+//
+- (void)testSimpleConnection
 {
-    STFail(@"Unit tests are not implemented yet in NSURLRequest+BasicAuthenticationTests");
+    NSString            *urlString      = nil;
+    NSURL               *url            = nil;
+    NSURLRequest        *request        = nil;
+    NSData              *data           = nil;
+    NSURLResponse       *response       = nil;
+    NSError             *error          = nil;
+    
+    urlString = [[NSString alloc] initWithFormat:@"http://user:secret@127.0.0.1:%d/index.html", [[self httpServer] listeningPort] ];
+    url = [NSURL URLWithString:urlString];
+    request = [NSURLRequest requestWithURL:url];
+    data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    STAssertNil(error, @"Server returned error");
+}
+
+// This should fail
+- (void)testSimpleConnectionAuthenticationFail
+{
+    NSString            *urlString      = nil;
+    NSURL               *url            = nil;
+    NSURLRequest        *request        = nil;
+    NSData              *data           = nil;
+    NSURLResponse       *response       = nil;
+    NSError             *error          = nil;
+    
+    urlString = [[NSString alloc] initWithFormat:@"http://user:secretzz@127.0.0.1:%d/index.html", [[self httpServer] listeningPort] ];
+    url = [NSURL URLWithString:urlString];
+    request = [NSURLRequest requestWithURL:url];
+    data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    STAssertNotNil(error, @"Server did not return error");
+}
+
+// Tests our category
+- (void)testBasicAuthentication
+{
+    NSString            *urlString      = nil;
+    NSURL               *url            = nil;
+    NSMutableURLRequest *request        = nil;
+    NSData              *data           = nil;
+    NSURLResponse       *response       = nil;
+    NSError             *error          = nil;
+    
+    urlString = [[NSString alloc] initWithFormat:@"http://127.0.0.1:%d/index.html", [[self httpServer] listeningPort] ];
+    url = [NSURL URLWithString:urlString];
+    request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPBasicAuthUsername:@"user" password:@"secret"];
+    data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    STAssertNil(error, @"Server returned error");
+}
+
+// Demonstrates using an NSURLCredential for basic authentication.
+- (void)testURLCredential
+{
+    NSString                *urlString          = nil;
+    NSURL                   *url                = nil;
+    NSMutableURLRequest     *request            = nil;
+    NSData                  *data               = nil;
+    NSURLResponse           *response           = nil;
+    NSError                 *error              = nil;
+    NSURLProtectionSpace    *protectionSpace    = nil;
+    NSURLCredential         *credential         = nil;
+    
+    urlString = [[NSString alloc] initWithFormat:@"http://127.0.0.1:%d/index.html", [[self httpServer] listeningPort] ];
+    url = [NSURL URLWithString:urlString];
+    
+    credential = [NSURLCredential credentialWithUser:@"user" password:@"secret" persistence: NSURLCredentialPersistencePermanent];
+    protectionSpace = [[NSURLProtectionSpace alloc] initWithHost:[url host] port:[[url port] integerValue] protocol:[url scheme] realm:nil authenticationMethod:NSURLAuthenticationMethodHTTPBasic];
+    [[NSURLCredentialStorage sharedCredentialStorage] setCredential:credential forProtectionSpace:protectionSpace];
+
+    data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    [[NSURLCredentialStorage sharedCredentialStorage] removeCredential:credential forProtectionSpace:protectionSpace];
+    [protectionSpace release];
+
+    STAssertNil(error, @"Server returned error");
 }
 
 @end
